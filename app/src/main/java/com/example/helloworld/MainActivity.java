2baf383eef295e779fe0d620e61bf193eadc2a64
GwonYuri <21900048@handong.edu>
2021-07-01 13:09:47 +0900

    구현완료

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.helloworld;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
         setContentView(R.layout.layout);
+
+        textView =findViewById(R.id.txtCount);
+    }
+
+    public void onToastClick(View view) {
+
+        // 메시지박스 띄우기
+        Toast.makeText(this, "Hello Toast", Toast.LENGTH_SHORT).show();
+    }
+    public void onCountClick(View view) {
+
+        //count++ 하면서 textview에 보이
+        count++;
+        textView.setText(count + "");
     }
 }

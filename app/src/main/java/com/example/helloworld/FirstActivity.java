1a4d560effb04fb47a207ac06e0b07617366a58d
GwonYuri <21900048@handong.edu>
2021-07-02 10:10:26 +0900

    intent commit

--- app/src/main/java/com/example/helloworld/FirstActivity.java
+++ app/src/main/java/com/example/helloworld/FirstActivity.java
@@ -5,44 +5,77 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FirstActivity extends AppCompatActivity {
-    Button b1;
-    Button b2;
-    EditText e1;
+    Button b1, b2, b3;
+    EditText e1, e2;
+    TextView tv1;
+
+    private final static int REQUEST_CODE_1 = 1000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
-        b1 = findViewById(R.id.btnCall);
+
+        b1= findViewById(R.id.btnCall);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 callSecondActivity();
             }
         });
+
+
         b2 = findViewById(R.id.btnMsg);
-        e1=findViewById(R.id.etMsg);
+        e1 = findViewById(R.id.etMsg);
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 callSecondActivityWithMsg();
             }
         });
+
+        b3 = findViewById(R.id.btnMsg3);
+        e2 = findViewById(R.id.etMsg3);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callSecondActivityWithMsg3();
+            }
+        });
+
+    }
+
+    private void callSecondActivityWithMsg3() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("msg", e2.getText().toString());
+        startActivityForResult(intent, REQUEST_CODE_1);
     }
 
     private void callSecondActivityWithMsg() {
-        Intent intent =new Intent(this, SecondActivity.class);
-        intent.putExtra("msg",e1.getText().toString());
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("msg", e1.getText().toString());
         startActivity(intent);
     }
 
     private void callSecondActivity() {
-        // 명시적 Intent
-        Intent intent =new Intent(this, SecondActivity.class);
+        Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE_1){
+            if(resultCode == RESULT_OK){
+                String msg = data.getStringExtra("msg");
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/helloworld/SecondActivity.java
+++ app/src/main/java/com/example/helloworld/SecondActivity.java
@@ -4,20 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
-    Button b1;
+    Button b1, b2;
     TextView tv1;
+    EditText e1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        init();
+    }
 
-        b1=findViewById(R.id.btnBack);
+    private void init(){
+        b1 = findViewById(R.id.btnBack);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,11 +30,30 @@ public class SecondActivity extends AppCompatActivity {
             }
         });
 
-        tv1=findViewById(R.id.tvMsg);
-        Intent intent=getIntent();
-        String msg=intent.getStringExtra("msg");
+
+        tv1 = findViewById(R.id.tvMsg);
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra("msg");
         tv1.setText(msg);
+
+
+        b2 = findViewById(R.id.btnMsg);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callSendMsg();
+            }
+        });
+        e1 =findViewById(R.id.etMsg);
+
     }
+    private void callSendMsg() {
+        Intent intent = new Intent(this, FirstActivity.class);
+        intent.putExtra("msg", e1.getText().toString());
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+
     private void callBack() {
         finish();
     }
